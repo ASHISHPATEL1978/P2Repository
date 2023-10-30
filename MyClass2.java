e5324a14e6e56e17ffa48cfa8e0997fc75374546
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-30 16:51:09 +0000

    P2 Commit

---
+++ MyClass2.java
@@ -0,0 +1,11 @@
+package mypackage2;
+
+public class MyClass2
+{
+        public static void main(String[] args)
+
+        {
+            System.out.println(" What are Qualifications Of Tester?");
+        }
+
+}
